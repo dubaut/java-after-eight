1820c99ea0fd6ee65d48a7738fe7c5ad6e3d5bd1
Nicolai Parlog <nipa@codefx.org>
2020-02-01 21:43:21 +0100

    Upgrade to Java 13 (with preview features)

--- cds.sh
+++ cds.sh
@@ -3,13 +3,13 @@ mkdir cds
 
 printf "\n--- NO CDS ---\n\n"
 printf ">> deactivating CDS\n"
-./stats-time.sh java -Xlog:class+load:file=cds/off.log \
+./stats-time.sh java --enable-preview -Xlog:class+load:file=cds/off.log \
 	-Xshare:off \
 	-cp jars/genealogy.jar:jars/genealogists.jar org.codefx.java_after_eight.Main
 
 printf "\n--- JDK CDS ---\n\n"
 printf ">> using default archive\n"
-./stats-time.sh java -Xlog:class+load:file=cds/jdk.log \
+./stats-time.sh java --enable-preview -Xlog:class+load:file=cds/jdk.log \
 	-cp jars/genealogy.jar:jars/genealogists.jar org.codefx.java_after_eight.Main
 
 
@@ -22,7 +22,7 @@ java -XX:+UnlockDiagnosticVMOptions -Xshare:dump \
 	-XX:SharedClassListFile=cds/classes.lst -XX:SharedArchiveFile=cds/app.jsa \
 	-cp jars/genealogy.jar:jars/genealogists.jar
 printf "\n>> using archive...\n"
-./stats-time.sh java -Xlog:class+load:file=cds/app.log \
+./stats-time.sh java --enable-preview -Xlog:class+load:file=cds/app.log \
 	-XX:+UnlockDiagnosticVMOptions -Xshare:on \
 	-XX:SharedArchiveFile=cds/app.jsa \
 	-cp jars/genealogy.jar:jars/genealogists.jar org.codefx.java_after_eight.Main
