afc353b8d7621a5dfc4b4876a0931228671f6230
Nicolai Parlog <nipa@codefx.org>
2020-02-01 08:48:25 +0100

    Use `Collectors::teeing`

--- genealogy/src/main/java/org/codefx/java_after_eight/genealogy/Relation.java
+++ genealogy/src/main/java/org/codefx/java_after_eight/genealogy/Relation.java
@@ -3,12 +3,16 @@ package org.codefx.java_after_eight.genealogy;
 import org.codefx.java_after_eight.article.Article;
 import org.codefx.java_after_eight.genealogist.TypedRelation;
 
+import java.util.Arrays;
 import java.util.Objects;
 import java.util.stream.Stream;
 
 import static java.lang.Math.round;
-import static java.lang.String.format;
 import static java.util.Objects.requireNonNull;
+import static java.util.stream.Collectors.averagingDouble;
+import static java.util.stream.Collectors.mapping;
+import static java.util.stream.Collectors.teeing;
+import static org.codefx.java_after_eight.Utils.collectEqualElement;
 
 public class Relation {
 
@@ -26,10 +30,14 @@ public class Relation {
 	}
 
 	static Relation aggregate(Stream<TypedRelation> typedRelations, Weights weights) {
-		return typedRelations
-				.map(relation -> new UnfinishedRelation(relation, weights.weightOf(relation.type())))
-				.reduce(UnfinishedRelation::fold)
-				.map(UnfinishedRelation::finish)
+		return typedRelations.collect(
+				teeing(
+						mapping(
+								rel -> new Article[]{ rel.article1(), rel.article2() },
+								collectEqualElement(Arrays::equals)),
+						averagingDouble(rel -> rel.score() * weights.weightOf(rel.type())),
+						(articles, score) -> articles.map(arts -> new Relation(arts[0], arts[1], round(score)))
+				))
 				.orElseThrow(() -> new IllegalArgumentException("Can't create relation from zero typed relations."));
 	}
 
@@ -69,38 +77,6 @@ public class Relation {
 				", article2=" + article2.slug().value() +
 				", score=" + score +
 				'}';
-	}
-
-	private static class UnfinishedRelation {
-
-		private final Article article1;
-		private final Article article2;
-		private double scoreTotal;
-		private long scoreCount;
-
-		public UnfinishedRelation(TypedRelation relation, double weight) {
-			this.article1 = relation.article1();
-			this.article2 = relation.article2();
-			this.scoreTotal = relation.score() * weight;
-			this.scoreCount = 1;
-		}
-
-		public UnfinishedRelation fold(UnfinishedRelation other) {
-			if (article1 != other.article1)
-				throw new IllegalArgumentException(format(
-						"All typed relations must belong to the same article: %s vs %s", article1, other.article1));
-			if (article2 != other.article2)
-				throw new IllegalArgumentException(format(
-						"All typed relations must belong to the same article: %s vs %s", article2, other.article2));
-			scoreTotal += other.scoreTotal;
-			scoreCount += other.scoreCount;
-			return this;
-		}
-
-		public Relation finish() {
-			return new Relation(article1, article2, round(scoreTotal / scoreCount));
-		}
-
 	}
 
 }
