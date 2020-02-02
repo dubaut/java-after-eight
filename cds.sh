ffde76a7602ac5b5bc9d06b9788e037c9eef6b8c
Nicolai Parlog <nipa@codefx.org>
2020-02-02 09:22:19 +0100

    Use dynamically generated app CDS archive

--- cds.sh
+++ cds.sh
@@ -15,14 +15,10 @@ printf ">> using default archive\n"
 
 printf "\n--- APP CDS ---\n\n"
 printf ">> building archive...\n"
-java -XX:+UnlockDiagnosticVMOptions \
-	-XX:DumpLoadedClassList=cds/classes.lst \
+java --enable-preview \
+	-XX:ArchiveClassesAtExit=cds/app.jsa \
 	-cp jars/genealogy.jar:jars/genealogists.jar org.codefx.java_after_eight.Main
-java -XX:+UnlockDiagnosticVMOptions -Xshare:dump \
-	-XX:SharedClassListFile=cds/classes.lst -XX:SharedArchiveFile=cds/app.jsa \
-	-cp jars/genealogy.jar:jars/genealogists.jar
 printf "\n>> using archive...\n"
 ./stats-time.sh java --enable-preview -Xlog:class+load:file=cds/app.log \
-	-XX:+UnlockDiagnosticVMOptions -Xshare:on \
 	-XX:SharedArchiveFile=cds/app.jsa \
 	-cp jars/genealogy.jar:jars/genealogists.jar org.codefx.java_after_eight.Main
